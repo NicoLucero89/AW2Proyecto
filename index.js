fdc3694d4cfa9d0de493cae07914d7846ac159b9
NicoLucero89 <168114935+NicoLucero89@users.noreply.github.com>
2024-10-07 13:42:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { readFile, writeFile } from 'fs/promises';
 import userRouter from './routes/usuario.routes.js';
 import itemsRouter from './routes/productos.routes.js';
 import saleRouter from './routes/ventas.routes.js';
+import postRouter from './routes/post.routes.js'
 
 const app = express();
 const port = 3000;
@@ -13,10 +14,19 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`Servidor levantado en puerto ${port}`);
 });
-
+// rutas de API
 app.use('/user', userRouter);
 app.use('/item', itemsRouter);
 app.use('/sale', saleRouter);
+app.use('/posts', postRouter);
+
+/*PARA LEVANTAR NUESTRO FRONT-END */
+app.use(express.static('./public'))
+
+
+/*RUTAS DE END-POINT */
+app.use('/user', userRouter)
+app.use('/posts', postRouter)// para los post
 
 
 
